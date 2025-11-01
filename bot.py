708d8d5a760c1ad7791d3f8c8127cd2c5277074c
holybiber <samuel@holydevelopers.net>
2025-11-01 21:10:37 +0100

    Stage 0 :-)

--- bot.py
+++ bot.py
@@ -4,14 +4,35 @@ import sys, json, random
 random.seed(1)
 first_tick = True
 
+move = "N"
 for line in sys.stdin:
     data = json.loads(line)
     if first_tick:
         config = data.get("config", {})
         width = config.get("width")
         height = config.get("height")
-        print(f"Random walker (Python) launching on a {width}x{height} map",
+        print(f"Elisa launching on a {width}x{height} map",
               file=sys.stderr, flush=True)
-    move = random.choice(["N", "S", "E", "W"])
+
+    unspos_x = data.get("bot")[0]
+    unspos_y = data.get("bot")[1]
+    gem = data.get("visible_gems")
+    if len(gem) == 0:
+        move = random.choice(["N", "S", "E", "W"])
+    else:
+        gempos_x = gem[0]["position"][0]
+        gempos_y = gem[0]["position"][1]
+        if gempos_x < unspos_x:
+            move = "W"
+        elif gempos_x > unspos_x:
+            move = "E"
+        elif gempos_y > unspos_y:
+            move = "S"
+        elif gempos_y < unspos_y:
+            move = "N"
+
+        print(f"wir x:{unspos_x}, y:{unspos_y}; Edelstein x: {gempos_x}, y: {gempos_y}", file=sys.stderr, flush=True)
+
     print(move, flush=True)
+
     first_tick = False
